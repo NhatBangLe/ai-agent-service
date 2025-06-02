4f005087c821a4792004df90043eb659894c96f0
NhatBangLe <bangle37568@gmail.com>
2025-06-02 14:08:35 +0700

    refactor code and add more endpoints

--- src/route/image.py
+++ src/route/image.py
@@ -1,3 +1,4 @@
+import datetime
 import os.path
 import typing
 from pathlib import Path
@@ -6,12 +7,12 @@ from uuid import UUID, uuid4
 from fastapi import APIRouter, UploadFile, status
 from sqlmodel import Session, select
 
-from ..dependency import SessionDep, DownloadGeneratorDep, PagingQuery, PagingParams
 from .label import get_label
 from ..data.dto import ImagePublic
-from ..data.model import Image, Label, User, LabeledImage
+from ..data.model import Image, User, LabeledImage
+from ..dependency import SessionDep, DownloadGeneratorDep, PagingQuery, PagingParams
 from ..util.error import NotFoundError
-from ..util.main import strict_uuid_parser, SecureDownloadGenerator, FileInformation
+from ..util.main import strict_uuid_parser, SecureDownloadGenerator, FileInformation, DEFAULT_TIMEZONE
 
 DEFAULT_SAVE_DIRECTORY = "/resource"
 save_image_directory = os.getenv("SAVE_IMAGE_DIRECTORY", DEFAULT_SAVE_DIRECTORY)
@@ -35,12 +36,25 @@ def get_image_download_token(image_id: UUID, session: Session, generator: Secure
 
 
 # noinspection PyTypeChecker,PyComparisonWithNone
+def get_images_by_label_id(label_id: UUID, params: PagingParams, session: Session) -> list[Image]:
+    statement = (select(Image)
+                 .join(LabeledImage, LabeledImage.image_id == Image.id)
+                 .where(LabeledImage.label_id == label_id)
+                 .offset(params.offset)
+                 .limit(params.limit)
+                 .order_by(LabeledImage.created_at))
+    results = session.exec(statement)
+    return list(results.all())
+
+
+# noinspection PyTypeChecker,PyComparisonWithNone
 def get_unlabeled_images(params: PagingParams, session: Session) -> list[Image]:
     statement = (select(Image)
-                 .join(LabeledImage, isouter=True)
+                 .join(LabeledImage, LabeledImage.image_id == Image.id, isouter=True)
                  .where(LabeledImage.label_id == None)
                  .offset(params.offset)
-                 .limit(params.limit))
+                 .limit(params.limit)
+                 .order_by(LabeledImage.created_at))
     results = session.exec(statement)
     return list(results.all())
 
@@ -56,6 +70,7 @@ async def save_image(user_id: UUID, file: UploadFile, session: Session) -> UUID:
         db_user = User(id=user_id)
     db_image = Image(
         id=image_id,
+        created_at=datetime.datetime.now(DEFAULT_TIMEZONE),
         name=file.filename,
         mime_type=file.content_type,
         save_path=save_path,
@@ -71,9 +86,18 @@ def assign_label_to_image(image_id: UUID, label_id: int, session: Session):
     db_image = get_image(image_id, session)
     db_label = get_label(label_id, session)
 
-    db_image.has_labels.append(typing.cast(Label, db_label))
-    session.add(db_image)
+    db_labeled_image = LabeledImage(
+        label=db_label,
+        image=db_image,
+        created_at=datetime.datetime.now(DEFAULT_TIMEZONE)
+    )
+    session.add(db_labeled_image)
     session.commit()
+
+
+def delete_image(image_id: UUID, session: Session):
+    db_image = get_image(image_id, session)
+    session.delete(db_image)
 
 
 router = APIRouter(
@@ -99,6 +123,11 @@ async def get_information(image_id: str, session: SessionDep):
     return get_image(image_id=image_uuid, session=session)
 
 
+@router.get("/{label_id}/label", response_model=list[ImagePublic], status_code=status.HTTP_200_OK)
+async def get_by_label_id(label_id: str, params: PagingQuery, session: SessionDep):
+    return get_images_by_label_id(label_id=strict_uuid_parser(label_id), params=params, session=session)
+
+
 @router.get("/unlabeled", response_model=list[ImagePublic], status_code=status.HTTP_200_OK)
 async def get_unlabeled(params: PagingQuery, session: SessionDep):
     return get_unlabeled_images(params=params, session=session)
@@ -110,7 +139,13 @@ async def upload(user_id: str, file: UploadFile, session: SessionDep) -> str:
     return str(uploaded_image_id)
 
 
-@router.post("/{image_id}/assign/{label_id}", status_code=status.HTTP_201_CREATED)
+@router.post("/{image_id}/assign/{label_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def assign_label(image_id: str, label_id: int, session: SessionDep) -> None:
     image_uuid = strict_uuid_parser(image_id)
     assign_label_to_image(session=session, image_id=image_uuid, label_id=label_id)
+
+
+@router.delete("/{image_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete(image_id: str, session: SessionDep) -> None:
+    image_uuid = strict_uuid_parser(image_id)
+    delete_image(image_id=image_uuid, session=session)
