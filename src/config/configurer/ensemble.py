783bf82a75d593e6f21d04fc69d1f1bc2d834d41
NhatBangLe <bangle37568@gmail.com>
2025-06-25 17:05:21 +0700

    refactor code

---
+++ src/config/configurer/ensemble.py
@@ -0,0 +1,64 @@
+import datetime
+
+from langchain.retrievers import EnsembleRetriever
+from langchain_core.tools import create_retriever_tool
+
+from src.config.configurer import RetrieverConfigurer
+from src.util.constant import DEFAULT_TIMEZONE
+
+
+class EnsembleRetrieverConfigurer(RetrieverConfigurer):
+    _retriever: EnsembleRetriever | None = None
+    _last_modified: datetime.datetime | None = None
+
+    RETRIEVER_NAME = "ensemble_retriever"
+    RETRIEVER_DESCRIPTION = (
+        "A highly robust and comprehensive tool designed to retrieve the most relevant and "
+        "accurate information from a vast knowledge base by combining multiple advanced search algorithms."
+        "**USE THIS TOOL WHENEVER THE USER ASKS A QUESTION REQUIRING EXTERNAL KNOWLEDGE,"
+        "FACTUAL INFORMATION, CURRENT EVENTS, OR DATA BEYOND YOUR INTERNAL TRAINING.**"
+        "**Examples of when to use this tool:**"
+        "- \"the capital of France?\""
+        "- \"the history of the internet.\""
+        "- \"the latest developments in AI?\""
+        "- \"quantum entanglement.\""
+        "**Crucially, use this tool for any query that cannot be answered directly from your"
+        "pre-trained knowledge, especially if it requires up-to-date, specific, or detailed factual data.**"
+        "The tool takes a single, concise search query as input."
+        "If you cannot answer after using this tool, you can use another tool to retrieve more information.")
+
+    def configure(self, **kwargs):
+        if "retrievers" not in kwargs:
+            raise ValueError(f'Must provide retrievers to configure EnsembleRetriever')
+        if "weights" not in kwargs:
+            raise ValueError(f'Must provide weights to configure EnsembleRetriever')
+        retrievers = kwargs["retrievers"]
+        weights = kwargs["weights"]
+
+        if retrievers is None or len(retrievers) == 0 or weights is None or len(weights) == 0:
+            return
+
+        self._retriever = EnsembleRetriever(retrievers=retrievers, weights=weights)
+        self._last_modified = datetime.datetime.now(DEFAULT_TIMEZONE)
+
+    async def async_configure(self, **kwargs):
+        self.configure(**kwargs)
+
+    def destroy(self, **kwargs):
+        pass
+
+    async def async_destroy(self, **kwargs):
+        self.destroy()
+
+    @property
+    def tool(self):
+        if self._retriever is None:
+            return None
+        return create_retriever_tool(
+            self._retriever,
+            name=self.RETRIEVER_NAME,
+            description=self.RETRIEVER_DESCRIPTION)
+
+    @property
+    def retriever(self):
+        return self._retriever
